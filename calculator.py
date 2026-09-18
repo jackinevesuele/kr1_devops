1b59f2dc5bd06311bdda949e500c0922086ac05f
Босарев Евгений <bosarev.e.v@edu.mirea.ru>
2026-09-18 09:46:30 +0300

    feat: add multiply and divide (experimental)

--- calculator.py
+++ calculator.py
@@ -10,3 +10,11 @@ def subtract(a, b):
 
 
 IMPORTANT_FIX = True
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
